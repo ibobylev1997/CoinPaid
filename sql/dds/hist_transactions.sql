ffc40742c1a366e4bfce95c3f67d24674463a73b
ibobylev1997 <86730222+ibobylev1997@users.noreply.github.com>
2023-02-13 16:15:34 +0300

    Update hist_transactions.sql

--- sql/dds/hist_transactions.sql
+++ sql/dds/hist_transactions.sql
@@ -1,6 +1,6 @@
 WITH cte AS (
     SELECT 
-         id
+         id_tran
         ,currency
         ,timestamp
         ,amount
@@ -8,13 +8,13 @@ WITH cte AS (
         ,created_at
         ,updated_at
     FROM stg.transactions
-    LIMIT 1 OVER (PARTITION BY id ORDER BY updated_at DESC)
+    LIMIT 1 OVER (PARTITION BY id_tran ORDER BY updated_at DESC)
 )
 
 MERGE INTO dds.hist_transactions A
 USING stg.transactions B
-ON (A.id=B.id)
+ON (A.id_tran=B.id_tran)
 WHEN MATCHED THEN 
     UPDATE SET A.last_txn_dt = B.updated_at
 WHEN NOT MATCHED THEN 
-    INSERT (id, first_txn, last_txn_dt) VALUES (B.id, B.created_at, B.updated_at)
+    INSERT (id_tran, first_txn, last_txn_dt) VALUES (B.id_tran, B.created_at, B.updated_at)
